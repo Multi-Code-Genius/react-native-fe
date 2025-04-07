db627422c9c1b8ef74571218a990a286dc8ff226
jay rajshakha <jayrajshakha73@gmail.com>
2025-04-07 15:46:09 +0530

    fixed npm version

--- android/app/src/screens/LoginScreen.tsx
+++ android/app/src/screens/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -7,33 +7,32 @@ import {
   KeyboardAvoidingView,
   Platform,
   Alert,
-  SafeAreaView,
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/MaterialIcons';
-import { StackScreenProps } from '@react-navigation/stack';
-import { useUserLogin } from '../api/auth/auth';
-import { useAuthStore } from '../store/authStore';
-
+import {StackScreenProps} from '@react-navigation/stack';
+import {useUserLogin} from '../api/auth/auth';
+import {useAuthStore} from '../store/authStore';
+import GoogleSignin from '../config/google';
 
 type Props = StackScreenProps<any, 'Login'>;
 
-const LoginScreen: React.FC<Props> = ({ navigation }) => {
+const LoginScreen: React.FC<Props> = ({navigation}) => {
   const [isPressed, setIsPressed] = useState(false);
-  const [data, setData] = useState({ email: '', password: '' });
-  const { mutate: login } = useUserLogin();
+  const [data, setData] = useState({email: '', password: ''});
+  const {mutate: login} = useUserLogin();
   const saveToken = useAuthStore(state => state.saveToken);
 
   const handleChange = (field: keyof typeof data, value: string) => {
-    setData(prev => ({ ...prev, [field]: value }));
+    setData(prev => ({...prev, [field]: value}));
   };
 
   const handleSubmit = async () => {
     if (!data.email || !data.password) {
-      Alert.alert("Error", "Please fill all fields");
+      Alert.alert('Error', 'Please fill all fields');
       return;
     }
     login(data, {
-      onSuccess: async ({ token }) => {
+      onSuccess: async ({token}) => {
         if (!token) {
           Alert.alert('Error', 'No token received.');
           return;
@@ -44,14 +43,26 @@ const LoginScreen: React.FC<Props> = ({ navigation }) => {
         Alert.alert('Error', 'Login failed. Please try again.');
       },
     });
-  }
+  };
 
+  const signInWithGoogle = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log('userInfo', userInfo);
+      const {idToken} = userInfo;
+
+      return data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
 
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-      className="flex-1 bg-white justify-center px-8"
-    >
+      className="flex-1 bg-white justify-center px-8">
       <View className="flex-1 justify-center items-center gap-16">
         <View className="flex-row gap-3 items-center justify-center w-full">
           <Ionicons name={'person'} size={35} color={'black'} />
@@ -88,20 +99,33 @@ const LoginScreen: React.FC<Props> = ({ navigation }) => {
           </View>
 
           <TouchableOpacity
-            className={`w-full h-12 rounded-lg flex justify-center mt-5 items-center ${isPressed ? 'bg-blue-500' : 'bg-blue-600'
-              }`}
+            className={`w-full h-12 rounded-lg flex justify-center mt-5 items-center ${
+              isPressed ? 'bg-blue-500' : 'bg-blue-600'
+            }`}
             activeOpacity={0.7}
             onPressIn={() => setIsPressed(true)}
             onPressOut={() => setIsPressed(false)}
-            onPress={handleSubmit}
-          >
+            onPress={handleSubmit}>
             <Text className="text-white text-[18px] font-semibold">LogIn</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            className={`w-full h-12 rounded-lg flex justify-center mt-5 items-center ${
+              isPressed ? 'bg-blue-500' : 'bg-blue-600'
+            }`}
+            activeOpacity={0.7}
+            onPressIn={() => setIsPressed(true)}
+            onPressOut={() => setIsPressed(false)}
+            onPress={signInWithGoogle}>
+            <Text className="text-white text-[18px] font-semibold">
+              Google Login
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
 
       <View className="absolute bottom-6 left-0 right-0 items-center border-t border-gray-300 pt-4">
-        <TouchableOpacity onPress={() => (navigation as any).navigate('SignUp')}>
+        <TouchableOpacity
+          onPress={() => (navigation as any).navigate('SignUp')}>
           <Text className="text-slate-600 text-lg">
             Don't have an account?{' '}
             <Text className="text-[#007BFF] font-bold">Sign Up</Text>
